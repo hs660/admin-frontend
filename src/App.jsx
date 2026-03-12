be2b1f25fe7c227eede678da836f6d65eab70998
Harsh Singh <rupeshshahu2003.com>
2026-03-12 17:09:09 +0530

    admin frontend initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './pages/Login';
 import AdminDashboard from './pages/Dashboard';
-import ProtectedRoute from './components/protect.route';
+import ProtectedRoute from './components/Protect.route.jsx';
 
 function App() {
   return (
